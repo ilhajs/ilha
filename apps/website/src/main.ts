422771f1e9cac9421fec8a147517a822d202d86c
Ryuz <ryuzer@proton.me>
2026-04-12 16:27:21 +0200

    fix(website): disable .d.ts check on ilha:pages

--- apps/website/src/main.ts
+++ apps/website/src/main.ts
@@ -2,6 +2,7 @@ import "./app.css";
 import "basecoat-css/all";
 import "shedit/editor.css";
 import { setupTheme } from "$lib/theme";
+// @ts-ignore just making sure .d.ts in CI is fine.
 import { pageRouter } from "ilha:pages";
 
 pageRouter.mount("#app");
