b4cabb977f489dbd7791f21626e4ff964584f7d7
Ryuz <ryuzer@proton.me>
2026-07-07 14:49:10 +0200

    revert(vite): remove dedupe

--- templates/nitro/vite.config.ts
+++ templates/nitro/vite.config.ts
@@ -6,9 +6,6 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [nitro(), pages(), tailwindcss()],
-  resolve: {
-    dedupe: ["ilha"],
-  },
   nitro: {
     serverDir: "./src",
   },
--- templates/vite/vite.config.ts
+++ templates/vite/vite.config.ts
@@ -5,9 +5,6 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [pages(), tailwindcss()],
-  resolve: {
-    dedupe: ["ilha"],
-  },
   server: {
     watch: {
       usePolling: true,
