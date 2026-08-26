7dc0f647b553f68d93d65ae09f54a5f8ed3ad214
Ryuz <ryuzer@proton.me>
2026-08-26 19:02:13 +0200

    chore(website): add blog

--- apps/website/components.ts
+++ apps/website/components.ts
@@ -2,4 +2,8 @@ import { defineComponents } from "blume";
 
 export default defineComponents({
   mdx: { Preview: "./src/components/Preview.astro" },
+  layout: {
+    Layout: "./src/components/layout/Layout.astro",
+    PageFooter: "./src/components/layout/PageFooter.astro",
+  },
 });
